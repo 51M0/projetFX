6957a1737e09ba0a466b0ea383af5ce27085dbb8
51m0 <lmouatassim@gmail.com>
2017-02-21 09:48:49 +0100

    commit
    premier commit

---
+++ src/application/Controlleur.java
@@ -0,0 +1,74 @@
+package application;
+
+
+
+import java.io.File;
+
+import javax.swing.JPopupMenu;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.FileChooser;
+
+public class Controlleur {
+	
+	@FXML
+	private Pane root;
+	private ContextMenu menuPic;
+	private MenuItem loadPicture;
+	
+	@FXML
+	private ImageView pic;
+	
+	public Controlleur(){		
+		menuPic=new ContextMenu();
+		loadPicture= new MenuItem("Load Picture");
+	menuPic.getItems().add(loadPicture);
+	loadPicture.setOnAction(new EventHandler<ActionEvent>() {
+		
+		@Override
+		public void handle(ActionEvent event) {
+			FileChooser fileChooser = new FileChooser(); 
+			if (fileChooser!= null){
+			fileChooser.setTitle("Open Resource File");
+			
+			fileChooser.getExtensionFilters().addAll(
+	                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+	                new FileChooser.ExtensionFilter("PNG", "*.png")
+	            );
+			fileChooser.showOpenDialog(menuPic);
+			
+			// TODO Auto-generated method stub
+			
+		}}
+	});
+	
+	
+		
+	}
+	@FXML
+	private void initialize(){
+	
+	}
+	
+	@FXML
+	public void clicked (MouseEvent e){
+		
+			menuPic.show(pic,e.getScreenX(), e.getScreenY());
+			System.out.println("pressed");
+		
+		
+		}
+	}
+
+
+
+
