7dad416330fe4f5e5f68a03e599a99d2739dd49c
sashor <sarah.ashor@hotmail.com>
2017-02-22 12:10:30 +0100

    redimenssionement de la fenetre

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = FXMLLoader.load(getClass().getResource("Gui.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,450,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
